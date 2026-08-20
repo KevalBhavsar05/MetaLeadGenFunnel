115823f530e13dfefa4e304588070d7e8d832fdc
KevalBhavsar05 <18kevalbhavsar@gmail.com>
2026-08-20 18:58:44 +0530

    updated

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+import jwt from "jsonwebtoken";
 export const adminAuthMiddleware = (req, res, next) => {
   try {
     const token = req.cookies.adminToken;
